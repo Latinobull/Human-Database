c12a69a5e9843a65f9be55ce90eedac903f8829b
Donnahue George <Donnahuegjr@gmail.com>
2021-09-25 11:14:32 -0400

    starting ui with kivy

---
+++ ui.py
@@ -0,0 +1,23 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
+
+
+class GridLayout(GridLayout):
+    # def __init__(self, **kwargs):
+    #     super().__init__(**kwargs)
+    #     b1 = Button(text='A')
+    pass
+
+
+class MainWidget(Widget):
+    pass
+
+
+class HumanGameApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    HumanGameApp().run()
